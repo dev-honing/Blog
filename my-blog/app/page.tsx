04a61611bb9823ac112737b6acb3180c3dba2a28
Ho Nyeong <dev.honing@gmail.com>
2024-02-07 15:13:40 +0900

    Design: MainPage 컴포넌트 구획

--- my-blog/app/page.tsx
+++ my-blog/app/page.tsx
@@ -1,11 +1,13 @@
 // app/page.tsx
 
-export default function Page() {
+export default function MainPage() {
   return (
     <div id="root">
-      <div id="header"></div>
-      <div id="container"></div>
-      <div id="footer"></div>
+      <div id="header">헤더 영역</div>
+      <div id="container">컨텐츠 영역</div>
+      <div id="footer">
+        <p>&copy; 2024 bhn1997.com. All rights reserved.</p>
+      </div>
       <div id="etc">
         <a href="/portfolio">포트폴리오 페이지</a>
       </div>
