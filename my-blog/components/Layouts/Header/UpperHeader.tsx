4e72b84a9be35528d94754d08b0e43c04e4b64d5
Ho Nyeong <dev.honing@gmail.com>
2024-02-15 11:59:16 +0900

    Refactor: Image 컴포넌트를 작성하는 것으로 변경

--- my-blog/components/Layouts/Header/UpperHeader.tsx
+++ my-blog/components/Layouts/Header/UpperHeader.tsx
@@ -1,7 +1,5 @@
 // components/Layouts/Header/UpperHeader.tsx
-interface UpperHeaderProps {
-  className?: string;
-}
+import Image from "next/image";
 
 export default function UpperHeader() {
   return (
@@ -9,14 +7,12 @@ export default function UpperHeader() {
       <div className="hostProfiles">
         {/* 작성자 정보 */}
         <div className="icon">
-          <picture>
-            <img
-              src="https://ca.slack-edge.com/T02JAN3TFEU-U05QNF4S8E4-d68d71b13d89-512"
-              alt="Slack"
-              width={30}
-              height={30}
-            />
-          </picture>
+          <Image
+            src="https://my-icons-svg.s3.ap-northeast-2.amazonaws.com/profile.svg"
+            alt="Profile"
+            width={30}
+            height={30}
+          />
         </div>
         <div className="ID">dev.honing</div>
       </div>
@@ -29,24 +25,20 @@ export default function UpperHeader() {
       <div className="externalLinks">
         {/* 외부 링크들 */}
         <a href="https://github.com/dev-honing">
-          <picture>
-            <img
-              src="https://my-icons-svg.s3.ap-northeast-2.amazonaws.com/SVG/GitHub.svg"
-              alt="GitHub"
-              width={30}
-              height={30}
-            />
-          </picture>
+          <Image
+            src="https://my-icons-svg.s3.ap-northeast-2.amazonaws.com/SVG/GitHub.svg"
+            alt="GitHub"
+            width={30}
+            height={30}
+          />
         </a>
         <a href="/portfolio">
-          <picture>
-            <img
-              src="https://my-icons-svg.s3.ap-northeast-2.amazonaws.com/portfolio.svg"
-              alt="Portfolio"
-              width={30}
-              height={30}
-            />
-          </picture>
+          <Image
+            src="https://my-icons-svg.s3.ap-northeast-2.amazonaws.com/portfolio.svg"
+            alt="Portfolio"
+            width={30}
+            height={30}
+          />
         </a>
       </div>
     </div>
