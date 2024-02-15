bead846c4faceeabe178b881f60b3a8e84828721
Ho Nyeong <dev.honing@gmail.com>
2024-02-15 10:49:36 +0900

    Refactor: Next.js의 방식에 맞게 picture 태그로 묶기

--- my-blog/components/Layouts/Header/UpperHeader.tsx
+++ my-blog/components/Layouts/Header/UpperHeader.tsx
@@ -4,12 +4,14 @@ export default function UpperHeader() {
       <div className="hostProfiles">
         {/* 작성자 정보 */}
         <div className="icon">
-          <img
-            src="https://ca.slack-edge.com/T02JAN3TFEU-U05QNF4S8E4-d68d71b13d89-512"
-            alt="Slack"
-            width={30}
-            height={30}
-          />
+          <picture>
+            <img
+              src="https://ca.slack-edge.com/T02JAN3TFEU-U05QNF4S8E4-d68d71b13d89-512"
+              alt="Slack"
+              width={30}
+              height={30}
+            />
+          </picture>
         </div>
         <div className="ID">dev.honing</div>
       </div>
@@ -20,20 +22,24 @@ export default function UpperHeader() {
       <div className="externalLinks">
         {/* 외부 링크들 */}
         <a href="https://github.com/dev-honing">
-          <img
-            src="https://my-icons-svg.s3.ap-northeast-2.amazonaws.com/SVG/GitHub.svg"
-            alt="GitHub"
-            width={30}
-            height={30}
-          />
+          <picture>
+            <img
+              src="https://my-icons-svg.s3.ap-northeast-2.amazonaws.com/SVG/GitHub.svg"
+              alt="GitHub"
+              width={30}
+              height={30}
+            />
+          </picture>
         </a>
         <a href="/portfolio">
-          <img
-            src="https://my-icons-svg.s3.ap-northeast-2.amazonaws.com/portfolio.svg"
-            alt="Portfolio"
-            width={30}
-            height={30}
-          />
+          <picture>
+            <img
+              src="https://my-icons-svg.s3.ap-northeast-2.amazonaws.com/portfolio.svg"
+              alt="Portfolio"
+              width={30}
+              height={30}
+            />
+          </picture>
         </a>
       </div>
     </div>
