722daac8bb97b0a77ce39243e0951eedc0e103ce
Ho Nyeong <dev.honing@gmail.com>
2024-02-15 10:50:23 +0900

    Design: 하단 헤더 하위의 초안 작성

---
+++ my-blog/components/Layouts/Header/LowerHeader.tsx
@@ -0,0 +1,16 @@
+export default function LowerHeader() {
+  return (
+    <div id="lowerHeader">
+      {/* 하단 헤더 */}
+      <div className="postSeries">
+        {/* 포스팅 시리즈 */}
+      </div>
+      <div className="postTitle">
+        {/* 포스팅 제목 */}
+      </div>
+      <div className="additionalFunctions">
+        {/* 부가 기능 */}
+      </div>
+    </div>
+  );
+}
