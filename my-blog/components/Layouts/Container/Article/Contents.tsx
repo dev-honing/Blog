95070829b13c27d9417f272f3c17cb6a79c07513
Ho Nyeong <dev.honing@gmail.com>
2024-02-19 17:07:54 +0900

    Feat: Contents 컴포넌트에 렌더링

--- my-blog/components/Layouts/Container/Article/Contents.tsx
+++ my-blog/components/Layouts/Container/Article/Contents.tsx
@@ -1,11 +1,13 @@
 // components/Layouts/Container/Article/Contents.tsx
 import { LongP, P } from "@/components/Contents/P";
+import DynamoFetch from "@/components/Contents/DynamoFetch";
 
 const Contents: React.FC = () => {
   return (
     <div className="article-contents">
       <P />
       {/* <LongP /> */}
+      <DynamoFetch />
     </div>
   );
 };
