533a0f71c2d91a9f680af6e39c619f4e9be726c1
Ho Nyeong <dev.honing@gmail.com>
2024-02-19 16:02:20 +0900

    Feat: DynamoDB에 쿼리를 보내고 반환하도록 query() 함수 로직 작성

--- my-blog/utils/DynamoDB.tsx
+++ my-blog/utils/DynamoDB.tsx
@@ -11,3 +11,12 @@ const ddb = new AWS.DynamoDB({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 });
+
+/**
+ * DynamoDB에 쿼리를 보내고 결과를 반환하는 함수
+ * @param {AWS.DynamoDB.QueryInput} params DynamoDB 쿼리 실행을 위한 파라미터
+ * @returns {Promise<AWS.DynamoDB.QueryOutput>} DynamoDB 쿼리의 결과를 나타내는 Promise
+ */
+export function query(params: AWS.DynamoDB.QueryInput): Promise<AWS.DynamoDB.QueryOutput> {
+  return ddb.query(params).promise();
+}
