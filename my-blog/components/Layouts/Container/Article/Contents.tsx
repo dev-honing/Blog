9119b3daded9204179d4b8bb575a72a51020d451
Ho Nyeong <dev.honing@gmail.com>
2024-02-19 20:50:43 +0900

    Fix: 동작하지 않는 컴포넌트 파일 삭제 및 이전으로 되돌리기

--- my-blog/components/Layouts/Container/Article/Contents.tsx
+++ my-blog/components/Layouts/Container/Article/Contents.tsx
@@ -1,14 +1,12 @@
 // components/Layouts/Container/Article/Contents.tsx
 "use client"
 import { LongP, P } from "@/components/Contents/P";
-import RenderTables from '@/components/Contents/RenderTables';
 
 const Contents: React.FC = () => {
   return (
     <div className="article-contents">
       <P />
       {/* <LongP /> */}
-      <RenderTables />
     </div>
   );
 };
