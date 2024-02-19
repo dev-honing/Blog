2ee1549e2badf9d74871711f731f91ad35fb0d74
Ho Nyeong <dev.honing@gmail.com>
2024-02-19 19:19:37 +0900

    Test: 리전을 명시하면, 콘솔 출력이 가능했다.

--- my-blog/test.js
+++ my-blog/test.js
@@ -3,19 +3,36 @@ const AWS = require('aws-sdk');
 AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION,
 });
 
-const dynamoDb = new AWS.DynamoDB();
+const ddb = new AWS.DynamoDB({
+  region: "ap-northeast-2"
+});
 
 const params = {
   TableName: 'Posts',
 };
 
-dynamoDb.query(params, (err, data) => {
+ddb.scan(params, (err, data) => {
   if (err) {
     console.error(err);
   } else {
     console.log(data.Items); // 쿼리 결과를 콘솔에 출력
   }
 });
+
+/**
+ * <출력 내용>
+ * [
+  {
+    postId: { S: '2' },
+    dateTime: { S: '2024-02-19 15:25' },
+    tags: { S: 'React.js' }
+  },
+  {
+    postId: { S: '1' },
+    dateTime: { S: '2024-02-19 15:25' },
+    tags: { S: 'Python' }
+  }
+]
+ */
