fa1fe7e2d9ee683ce31b6f4400d62151c006c545
Ho Nyeong <dev.honing@gmail.com>
2024-02-19 17:01:02 +0900

    Feat: DynamoDB에서 데이터를 가져오는 fetchData() 함수 정의

--- my-blog/utils/DynamoDB.tsx
+++ my-blog/utils/DynamoDB.tsx
@@ -20,3 +20,23 @@ const ddb = new AWS.DynamoDB({
 export function query(params: AWS.DynamoDB.QueryInput): Promise<AWS.DynamoDB.QueryOutput> {
   return ddb.query(params).promise();
 }
+
+/**
+ * DynamoDB에서 데이터를 가져오는 함수
+ * @param {string} postId 가져올 데이터의 postId
+ * @returns {Promise<any[]>} DynamoDB에서 가져온 데이터를 나타내는 Promise
+ */
+export async function fetchData(postId: string): Promise<any[]> {
+  try {
+    const params = {
+      TableName: "Posts", // 테이블명
+      KeyConditionExpression: "postId = :postId", // postId 속성이 특정 값과 일치하는지를 나타내는 조건
+      ExpressionAttributeValues: { ":postId": { S: postId.toString() } }, // 쿼리에 사용할 값으로 postId를 문자열 형태로 변환하여 설정
+    };
+    const result = await query(params); // query 함수 호출
+    return result.Items ?? [];
+  } catch (error) {
+    console.error("Error fetching data from DynamoDB:", error);
+    throw error;
+  }
+}
