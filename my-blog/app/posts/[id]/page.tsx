39e130b47933217c369461ecb45aa9c5eb15a5d7
Ho Nyeong <dev.honing@gmail.com>
2024-03-02 23:45:58 +0900

    Test: 동적 라우팅 구성 확인

---
+++ my-blog/app/posts/[id]/page.tsx
@@ -0,0 +1,7 @@
+export default function Articles(){
+  return <div id="articles">
+    <h3>아티클별 동적 라우팅</h3>
+    <span>현재 URL: </span>
+  </div> 
+  
+}
---
+++ my-blog/app/posts/page.tsx
@@ -0,0 +1,8 @@
+// app/posts/page.tsx
+
+const Posts = () => {
+  return <div className="posts">
+    <h3>포스트 메인</h3>
+  </div>
+}
+export default Posts;
