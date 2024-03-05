33ee9b3966f155b298e42a3a159b2a015502673a
Ho Nyeong <dev.honing@gmail.com>
2024-03-06 01:26:52 +0900

    Fix: 루트 레이아웃 수정 및 라우트한 URL에 적용 확인

--- my-blog/app/layout.tsx
+++ my-blog/app/layout.tsx
@@ -1,20 +1,28 @@
 // app/layout.tsx
-import "@/app/global.css"
+import "@/app/global.css";
 
 export const metadata = {
   // 제목과 설명 작성
-  title: '재미 주도 개발',
-  description: '초보 블로거',
-}
+  title: "재미 주도 개발",
+  description: "초보 블로거",
+};
 
 export default function RootLayout({
-  children
+  children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="ko">
-      <body>{children}</body>
+      <body>
+        <header style={{ backgroundColor: "lightblue", padding: "1rem" }}>
+          <p>Header</p>
+        </header>
+        {children}
+        <footer style={{ backgroundColor: "ghostwhite", padding: "1rem" }}>
+          <p>Footer</p>
+        </footer>
+      </body>
     </html>
-  )
+  );
 }
