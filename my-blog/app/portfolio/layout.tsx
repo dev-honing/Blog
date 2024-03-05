3aade0e98aef545341ce811c1f71b3992b25898c
Ho Nyeong <dev.honing@gmail.com>
2024-03-06 01:47:33 +0900

    Feat: 포트폴리오 페이지에 중첩 레이아웃 설정 및 적용

--- my-blog/app/portfolio/layout.tsx
+++ my-blog/app/portfolio/layout.tsx
@@ -1,5 +1,15 @@
 // app/portfolio/layout.tsx
 
-export default function PortfolioLayout({children}: {children: React.ReactNode}) {
-  return <section>{children}</section>
+export default function PortfolioLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      {children}
+      <h2>팀 프로젝트</h2>
+      <h2>개인 프로젝트</h2>
+    </>
+  );
 }
