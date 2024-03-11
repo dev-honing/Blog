8e48b9045e9af005e342041ba49049d1ddfc9878
Ho Nyeong <dev.honing@gmail.com>
2024-03-11 11:46:37 +0900

    Bad: API Route를 이용해 Fetch를 시도했지만 404 에러

--- my-blog/app/pages/posts/page.tsx
+++ my-blog/app/pages/posts/page.tsx
@@ -1,10 +1,12 @@
 // app/posts/page.tsx
 import { ToHome } from '@/components/Links/Links';
+import FetchRDS from '../../../components/FetchRDS';
 
 const Posts = () => {
   return <div className="posts">
     <ToHome />
     <h3>포스트 메인</h3>
+    <FetchRDS />
   </div>
 }
 export default Posts;
