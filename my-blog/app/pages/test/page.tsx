2b2eb1fe3e74ac140f138083caf1a9275858c812
Ho Nyeong <dev.honing@gmail.com>
2024-03-15 00:29:27 +0900

    Bad: 문자열 Greet조차 Fetch 하지 못한다
    - 404 GET ERROR

---
+++ my-blog/app/pages/test/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+// app/pages/test/page.tsx
+import FetchHello from './../../../components/FetchHello';
+
+const TestPage = () => {
+  return (
+    <div>
+      <h1>Test Page</h1>
+      <FetchHello />
+    </div>
+  );
+}
+
+export default TestPage;
