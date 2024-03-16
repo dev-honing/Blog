d20ec0e56c3c0243971e845074f970a34749a522
Ho Nyeong <dev.honing@gmail.com>
2024-03-16 20:24:57 +0900

    Delete: API Route 관련 로직 모두 삭제

--- my-blog/app/pages/api/greet.ts
+++
@@ -1,6 +0,0 @@
-// app/pages/api/greet.ts
-import { NextApiRequest, NextApiResponse } from 'next';
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ message: 'Hello, Next API Routes!' });
-}
--- my-blog/app/pages/test/page.tsx
+++ my-blog/app/pages/test/page.tsx
@@ -1,12 +1,10 @@
 'use client'
 // app/pages/test/page.tsx
-import FetchHello from './../../../components/FetchHello';
 
 const TestPage = () => {
   return (
     <div>
       <h1>Test Page</h1>
-      <FetchHello />
     </div>
   );
 }
