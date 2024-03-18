28f8639d49f73a592cd3d2983ce15dae2475d550
Ho Nyeong <dev.honing@gmail.com>
2024-03-18 11:04:36 +0900

    Feat: App Router 방식으로 원하는 엔드 포인트 생성 및 렌더링 성공

--- my-blog/app/test/page.tsx
+++ my-blog/app/test/page.tsx
@@ -1,10 +1,41 @@
+'use client'
 // app/test/page.tsx
+import { useEffect, useState } from 'react';
+
+type TestData = {
+  id: number;
+  category: string;
+  dateTime: string;
+  title: string;
+  tags: string[];
+};
 
 const TestPage = () => {
+  const [data, setData] = useState<TestData | null>(null);
+
+  useEffect(() => {
+    const getTest = async () => {
+      const res = await fetch('http://localhost:3000/api/test');
+      const json = await res.json();
+      setData(json.test);
+    };
+
+    getTest();
+  }, []);
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <main>
       <h1>Test Page</h1>
-      {/* Fetch 컴포넌트 */}
+      <div>
+        <h2>{data.title}</h2>
+        <p>{data.category}</p>
+        <p>{data.dateTime}</p>
+        <p>{data.tags.join(', ')}</p>
+      </div>
     </main>
   );
 };
